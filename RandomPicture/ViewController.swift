062c0061b48eb85db09fe02793842ef53ea75e90
KEKSman <bassman7009@gmail.com>
2020-10-14 11:18:18 +0500

    Random picture. Test build

--- RandomPicture/ViewController.swift
+++ RandomPicture/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let imageUrl = "https://picsum.photos/200"
+    @IBOutlet var button: UIButton!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func actionButton() {
+        fetchImage()
+    }
+    private func fetchImage(){
+        guard let url = URL(string: imageUrl) else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if  let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let response = response {
+                print(response)
+            }
+            if let data = data, let image = UIImage(data: data){
+                DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+        }.resume()
+    }
 }
 
